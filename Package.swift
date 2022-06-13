9adc88d1d820641a05526cb439fe3a348fd3e704
Lee <16902456r@connect.polyu.hk>
2022-06-13 15:54:23 +0800

    Package the Source

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SDK",
     platforms: [
-    .iOS(.v14)
+        .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -21,11 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SDK",
-            dependencies: []),
-        .testTarget(
-            name: "SDKTests",
-            dependencies: ["SDK"]),
+        .binaryTarget(
+                    name: "SDK",
+                    path: "./Sources/SDK.xcframework")
     ]
 )
---
+++ Sources/SDK.xcframework/ios-arm64/SDK.framework/Headers/SDK.h
@@ -0,0 +1,18 @@
+//
+//  SDK.h
+//  SDK
+//
+//  Created by Yaxin Li on 13/6/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDK.
+FOUNDATION_EXPORT double SDKVersionNumber;
+
+//! Project version string for SDK.
+FOUNDATION_EXPORT const unsigned char SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK/PublicHeader.h>
+
+
